4aafa64ac278973f5d3d4281bdcbc4c42cb58675
rafaelcapato-eng <rafael.capato@escola.pr.gov.br>
2026-09-01 15:20:41 +0000

    .

--- acesso-colchete.js
+++ acesso-colchete.js
@@ -9,7 +9,7 @@ console.log(
   `O nome do cliente é ${cliente["nome"]} e essa pessoa tem ${cliente["idade"]} anos.`
 );
 
-const chaves = ["nome", "idade", "cpf", "email"5];
+const chaves = ["nome", "idade", "cpf", "email", "altura"];
 
 chaves.forEach((chave) => {
   console.log(`A chave ${chave} tem valor ${cliente[chave]}`);
